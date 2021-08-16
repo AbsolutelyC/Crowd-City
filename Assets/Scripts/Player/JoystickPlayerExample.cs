880b38ca57a6441fa5951dbe86734524d609d438
AbsolutelyC <tungnvt152001@gmail.com>
2021-08-16 09:28:01 +0700

    Replace input movement with joystick

---
+++ Assets/Scripts/Player/JoystickPlayerExample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+
+    private void Awake()
+    {
+        variableJoystick.SetMode(JoystickType.Floating);
+    }
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
